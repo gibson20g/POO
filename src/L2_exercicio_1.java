53191af10a316bdf47e81dcd04f40355fc4c49a1
gibson20g <77676359+gibson20g@users.noreply.github.com>
2021-06-16 16:01:10 -0300

    Primeiros Exercicios em JAVA POO

--- src/L2_exercicio_1.java
+++ src/L2_exercicio_1.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class L2_exercicio_1 {
+    public static void main (String[] args){
+        Scanner dados = new Scanner(System.in);
+        System.out.println("IDENTIFICADOR DE NÚMEROS PARES E ÍMPARES");
+        System.out.println("Informe o número INTEIRO que deseja analisar: ");
+        double num = dados.nextDouble();
+
+        if (num/2 == 0) {
+            System.out.println("legal");
+        }else{
+            System.out.println("errado");
+            }
+
+    }
 }
